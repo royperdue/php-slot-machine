43c86c5386cdc70b69701689bf8c89882cfe7576
royperdue <roy.perdue.a@gmail.com>
2015-05-07 14:21:50 -0400

    -

--- web/DB_Connect.php
+++ web/DB_Connect.php
@@ -3,23 +3,15 @@ class DB_Connect
 {
 	public function __construct()
 	{
-		$db_hostname = 'localhost';
-		$db_database = 'rperdue';
-		$db_username = 'rperdue'; 
-		$db_password = 'A30029858';
-		
-		$connection = mysql_connect($db_hostname, $db_username, $db_password); 
-
-		if($this == FALSE) 
-		{
-			 echo '<script type="text/javascript">alert("Unable to connect to MySQL:");</script>';
-		}
-		mysql_select_db('rperdue');
-		if (mysql_select_db($db_database) == FALSE)
-		{
-			echo '<script type="text/javascript">alert("Unable to select MySQL DB:");</script>';
-		}
-		return $connection;
+		$dbopts = parse_url(getenv('DATABASE_URL'));
+		$app->register(new Herrera\Pdo\PdoServiceProvider(),
+  		array(
+    		'pdo.dsn' => 'pgsql:dbname='.ltrim($dbopts["path"],'/').';host='.$dbopts["host"],
+    		'pdo.port' => $dbopts["port"],
+    		'pdo.username' => $dbopts["user"],
+    		'pdo.password' => $dbopts["pass"]
+  			)
+		);
 	}
 }
 ?>
