09d450c921d9de5e6b924b30851a9aa0b0de393e
royperdue <roy.perdue.a@gmail.com>
2015-05-07 15:31:42 -0400

    -

--- web/DB_Connect.php
+++ web/DB_Connect.php
@@ -3,18 +3,15 @@ class DB_Connect
 {
 	public function __construct()
 	{
-		function pg_connection_string() 
-		{
-  			return "dbname=d6bkcb9sfqhi44 host=ec2-184-73-165-195.compute-1.amazonaws.com user=ptfcmkmdexdnzk password=dHqiWWZgdW5j49pHJad76px4e4 sslmode=require";
-		}
+		$connection = "pgsql:"
+    		. "host=ec2-184-73-165-195.compute-1.amazonaws.com;"
+    		. "dbname=d6bkcb9sfqhi44;"
+    		. "user=ptfcmkmdexdnzk;"
+    		. "port=5432;"
+    		. "sslmode=require;"
+    		. "password=dHqiWWZgdW5j49pHJad76px4e4";
  
-		return $connection = pg_connect(pg_connection_string());
-		
-		if (!$connection) 
-		{
-    		echo "Database connection error."
-    		exit;
-		}
+		return $connection = new PDO($connection);
 	}
 }
 ?>
