d83750b8b82d9ce26d1ec18305e5e4bb6d02d115
royperdue <roy.perdue.a@gmail.com>
2015-05-07 15:20:55 -0400

    -

--- web/DB_Connect.php
+++ web/DB_Connect.php
@@ -8,15 +8,13 @@ class DB_Connect
   			return "dbname=d6bkcb9sfqhi44 host=ec2-184-73-165-195.compute-1.amazonaws.com user=ptfcmkmdexdnzk password=dHqiWWZgdW5j49pHJad76px4e4 sslmode=require";
 		}
  
-		$connection = pg_connect(pg_connection_string());
+		return $connection = pg_connect(pg_connection_string());
 		
 		if (!$connection) 
 		{
     		echo "Database connection error."
     		exit;
 		}
- 
-		return $connection;
 	}
 }
 ?>
