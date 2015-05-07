aa058a918eb470a22ade9ed6460328b0cc9e8e73
royperdue <roy.perdue.a@gmail.com>
2015-05-07 17:43:20 -0400

    -

--- web/index.php
+++ web/index.php
@@ -336,6 +336,7 @@
 											 if(pg_num_rows($result) > 5)
 											 {
 											 	pg_query('TRUNCATE TABLE GAME');
+											 	pg_query('ALTER TABLE mytable AUTO_INCREMENT = 1');
 											 }
 											?>
 										</table>
